d572ebcf3b651aa4d56b03e72dc487b3a9b2a0a9
Janus Troelsen <janus@flipstone.com>
2022-10-19 10:36:55 -0500

    Address doctest v0.20.1 compatibility

--- src/Data/OpenApi.hs
+++ src/Data/OpenApi.hs
@@ -137,7 +137,10 @@ import Data.OpenApi.Internal
 -- >>> import Data.Proxy
 -- >>> import GHC.Generics
 -- >>> import qualified Data.ByteString.Lazy.Char8 as BSL
+-- >>> import Data.OpenApi.Internal
+-- >>> import Data.OpenApi.Internal.Schema
 -- >>> import Data.OpenApi.Internal.Utils
+-- >>> import Data.OpenApi.Lens
 -- >>> :set -XDeriveGeneric
 -- >>> :set -XOverloadedStrings
 -- >>> :set -XOverloadedLists
@@ -213,7 +216,7 @@ import Data.OpenApi.Internal
 -- BSL.putStrLn $ encodePretty $ (mempty :: OpenApi)
 --   & components . schemas .~ [ ("User", mempty & type_ ?~ OpenApiString) ]
 --   & paths .~
---     [ ("/user", mempty & get ?~ (mempty
+--     [ ("/user", mempty & Data.OpenApi.Lens.get ?~ (mempty
 --         & at 200 ?~ ("OK" & _Inline.content.at "application/json" ?~ (mempty & schema ?~ Ref (Reference "User")))
 --         & at 404 ?~ "User info not found")) ]
 -- :}
