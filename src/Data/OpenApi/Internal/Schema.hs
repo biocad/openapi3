c2e706b3886b0bba745ededd39ccccc41033ac96
Janus Troelsen <janus@flipstone.com>
2022-10-26 09:14:18 -0500

    Fix mtl-2.3 compat (#60)
    Co-authored-by: Martin Beták <matobet@gmail.com>
    
    Co-authored-by: Martin Beták <matobet@gmail.com>

--- src/Data/OpenApi/Internal/Schema.hs
+++ src/Data/OpenApi/Internal/Schema.hs
@@ -28,7 +28,7 @@ import Control.Lens hiding (allOf)
 import Data.Data.Lens (template)
 
 import Control.Monad
-import Control.Monad.Writer
+import Control.Monad.Writer hiding (First, Last)
 import Data.Aeson (Object (..), SumEncoding (..), ToJSON (..), ToJSONKey (..),
                    ToJSONKeyFunction (..), Value (..))
 import Data.Char
@@ -50,6 +50,7 @@ import Data.Proxy
 import Data.Scientific (Scientific)
 import Data.Fixed (Fixed, HasResolution, Pico)
 import Data.Set (Set)
+import Data.Semigroup
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as TL
 import Data.Time
