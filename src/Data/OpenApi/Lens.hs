588e6e30a8b58d1dacfdef54504a2905acb20b21
Arjun Kathuria <arjun.kathuria.ext@juspay.in>
2023-01-09 14:17:09 +0530

    Adds "Ixed" and "At" type-class instances for SecurityDefinitions type

--- src/Data/OpenApi/Lens.hs
+++ src/Data/OpenApi/Lens.hs
@@ -104,6 +104,11 @@ instance HasType NamedSchema (Maybe OpenApiType) where type_ = schema.type_
 -- Type family instances for SecurityDefinitions type
 type instance Index SecurityDefinitions = Text
 type instance IxValue SecurityDefinitions = SecurityScheme
+
+-- Type-Class instances for SecurityDefinitions type
+instance Ixed SecurityDefinitions where ix n = (coerced :: Lens' SecurityDefinitions (Definitions SecurityScheme)). ix n
+instance At   SecurityDefinitions where at n = (coerced :: Lens' SecurityDefinitions (Definitions SecurityScheme)). at n
+
 -- OVERLAPPABLE instances
 
 instance
